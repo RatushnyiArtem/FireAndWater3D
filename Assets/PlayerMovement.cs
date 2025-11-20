7a5647db26a3c380e7d04c8db64226c859b78e5f
Hasan0991 <gamzayevgasan098@gmail.com>
2025-11-20 17:19:56 +0100

    Initial commit with Unity project

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, 0, v) * speed;
+        rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
+    }
+}
